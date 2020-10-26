96401c725d05b0c81d26af25cc8dcbdc5fa97bdd
adiletcool <52336435+adiletcool@users.noreply.github.com>
2020-10-27 02:02:02 +0300

    trying to get folder on heroku

--- back.py
+++ back.py
@@ -9,7 +9,8 @@ columns = ['iso_code', 'date', 'location',
            'new_cases', 'total_cases', 'new_cases_per_million', 'total_cases_per_million',
            'new_deaths', 'total_deaths', 'new_deaths_per_million', 'total_deaths_per_million']
 last_data_url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
-_path = 'assets/data'
+app_path = '/app/'
+_path = app_path + 'assets/data'
 yesterday_str = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
 _filename = f'covid-data-{yesterday_str}.csv'
 _csv_files = os.listdir(_path)
